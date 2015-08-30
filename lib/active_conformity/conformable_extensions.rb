effd4cc1a4c2d64e33b5440171bb4709b1775f69
dandlezzz <danm@workwithopal.com>
2015-08-30 13:18:56 -0700

    version bump and uniq addition

--- lib/active_conformity/conformable_extensions.rb
+++ lib/active_conformity/conformable_extensions.rb
@@ -21,7 +21,7 @@ module ActiveConformity
       self.class.reflect_on_all_associations
       .select do |reflection|
         ActiveConformity::Conformable.where(conformist_type: self.class.name )
-        .pluck(:conformable_type)
+        .pluck(:conformable_type).uniq
         .include?(reflection.klass.name) rescue nil
       end.map(&:name)
       .flat_map{|relation_name| self.send(relation_name) }
@@ -30,6 +30,7 @@ module ActiveConformity
     def aggregate_conformity_set
       # still aren't finding custom validation methods
       acs ={}
+      return acs if !conformable_references.any? #need to think about this a little more
       conformable_references.each do |c|
         c = ActiveConformity::Conformable.find_by!(conformable_id: c.id, conformable_type: c.class.name).conformity_set
         c = JSON.parse(c) if c.is_a?(String)

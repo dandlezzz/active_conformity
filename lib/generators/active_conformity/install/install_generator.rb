f36c9018d497e69cdda6095fe901f501da4808fc
dandlezzz <danm@workwithopal.com>
2015-09-23 07:58:23 -0700

    generator work
    generator fun
    
    more generator fun
    
    more generator fun
    
    more generator fun
    
    more gnerator fixes
    
    more gnerator fixes
    
    more gen gen
    
    fix generator migration timestamp
    
    v bump
    
    gen gen gen
    
    cnt use jsonb in postgres < 9.4

--- lib/generators/active_conformity/install/install_generator.rb
+++ lib/generators/active_conformity/install/install_generator.rb
@@ -2,12 +2,44 @@ require 'rails/generators/active_record'
 module ActiveConformity
   module Generators
     class Install < Rails::Generators::Base
+      include Rails::Generators::Migration
       source_root File.expand_path('../templates', __FILE__)
-      desc "Creates the conformable model and table which stores the conformity sets as json \n
+      desc "Creates the conformable model and table which stores the conformity sets as json
       and the custom validation methods module in the lib directory."
 
-      def generate_model
-        generate("model", "Conformable","conformity_set:jsonb conformable_type:string conformable_id:integer conformist_type:string")
+      def self.source_root
+        @source_root ||= File.expand_path('../templates', __FILE__)
+      end
+
+      def self.next_migration_number(path)
+        @migration_number = Time.now.strftime("%Y%m%d%H%M%S")
+      end
+
+      def generate_migration
+        migration_template "active_conformity_migration.rb.erb", "db/migrate/#{migration_file_name}"
+      end
+
+      def migration_name
+        "create_conformables"
+      end
+
+      def migration_class_name
+        migration_name.camelize
+      end
+
+      def migration_file_name
+        "#{migration_name}.rb"
+      end
+
+      def conformity_set_type
+        database_adapters.fetch(ActiveRecord::Base.connection.adapter_name, 'json')
+      end
+
+      def database_adapters
+        {
+          'MySQL' => 'json',
+          'PostgreSQL' => 'json' # This can be jsonb for 9.4 and above
+        }
       end
 
       def create_custom_methods_module_file

09dd4081929b9944e8bb8c0213c795ab2bc4c938
dandlezzz <danm@workwithopal.com>
2015-09-23 07:50:55 -0700

    working on custom method generator

--- lib/generators/active_conformity/install/install_generator.rb
+++ lib/generators/active_conformity/install/install_generator.rb
@@ -2,10 +2,16 @@ require 'rails/generators/active_record'
 module ActiveConformity
   module Generators
     class Install < Rails::Generators::Base
-      desc "Creates the conformable model and table which stores the conformity sets as json"
+      source_root File.expand_path('../templates', __FILE__)
+      desc "Creates the conformable model and table which stores the conformity sets as json \n
+      and the custom validation methods module in the lib directory."
 
       def generate_model
         generate("model", "Conformable","conformity_set:jsonb conformable_type:string conformable_id:integer conformist_type:string")
+      end
+
+      def create_custom_methods_module_file
+        template "active_conformity_custom_validation_methods.rb.erb", "lib/active_conformity_custom_validation_methods.rb"
       end
     end
   end
